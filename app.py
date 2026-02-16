813c0f284896644d59ddb5949c4d5b6c472bed73
TJ Mike <michael2904kh@gmail.com>
2026-02-16 17:21:10 +0000

    started the app file

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from scipy.stats import norm
+import plotly.graph_objects as go
+from numpy import log, sqrt, exp
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+
+
